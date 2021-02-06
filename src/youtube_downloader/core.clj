1fae34ef382892c864a82a27f63e7871a2795cc7
James Vickers <jamesvickers19@gmail.com>
2021-02-06 16:53:54 -0700

    Working POST route that downloads file with given sections

--- src/youtube_downloader/core.clj
+++ src/youtube_downloader/core.clj
@@ -1,16 +1,16 @@
 (ns youtube-downloader.core
-  (:require [compojure.core :refer [defroutes GET]]
+  (:require [compojure.core :refer [defroutes GET POST]]
+            [ring.middleware.keyword-params :refer [wrap-keyword-params]]
+            [ring.middleware.json :refer [wrap-json-params]]
             [ring.adapter.jetty :as ring]
             [ring.util.response :refer [response]]
-            [youtube_downloader.download :as dl]
-            [youtube-downloader.section-files :as sect]
+            [youtube_downloader.download :refer [download-audio get-sections]]
+            [youtube-downloader.section-files :refer [section-file]]
             [clojure.data.json :as json]
             [ring.util.io :as ring-io]
-            [clojure.java.io :as io])
+            [clojure.java.io :as io]
+            [clojure.tools.trace :refer :all])
   (:import (java.util.zip ZipOutputStream ZipEntry)))
-
-; TODO start client code in clojure, put in this repo?
-; or keep using ReactJS?
 
 (defn zip-files
   "Returns an inputstream (piped-input-stream) to be used directly in Ring HTTP responses"
@@ -28,20 +28,25 @@
   [video-id]
   {:headers {"Content-type" "application/json"
              "Access-Control-Allow-Origin" "*"}
-   :body    (json/write-str (dl/get-sections video-id))})
+   :body    (json/write-str (get-sections video-id))})
 
 (defn download-files-zip
-  []
-  {:status 200
-   :headers {"Content-Type" "application/zip, application/octet-stream"
-             "Content-Disposition" (str "attachment; filename=\"files.zip\"")}
-   :body (zip-files ["C:\\Users\\james\\Downloads\\SPYDER550 - GODDD MODE.mp4"
-                     "C:\\Users\\james\\Downloads\\ViNi $AN - FOR REAL.mp4"])})
+  [{{:keys [video-id sections]} :params}]
+  (let [filename "C:\\Users\\james\\Downloads\\test" ; TODO make temporary dir
+        file (download-audio video-id filename)
+        downloaded-filename (.getAbsolutePath file)
+        sections (section-file downloaded-filename sections)]
+    {:status 200
+     :headers {"Content-Type" "application/zip, application/octet-stream"
+               "Content-Disposition" "attachment; filename=\"files.zip\""}
+     :body (zip-files sections)}))
+
+(defn json-handler [handler]
+  (-> handler wrap-keyword-params wrap-json-params))
 
 (defroutes routes
    (GET "/sections/:v" [v] (get-sections-handler v))
-   ; TODO post with body params ?
-   (GET "/download" [] (download-files-zip)))
+   (POST "/download" req ((json-handler download-files-zip) req)))
 
 (defn -main []
   (ring/run-jetty #'routes {:port 8080 :join? false}))
