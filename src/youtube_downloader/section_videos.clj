f9643782c618ee5ccada8dfddfa4bdcdc4c7564a
James Vickers <jamesvickers19@gmail.com>
2021-04-24 12:26:46 -0600

    Remove dead code from section-videos namespace

--- src/youtube_downloader/section_videos.clj
+++ src/youtube_downloader/section_videos.clj
@@ -1,8 +1,6 @@
 (ns youtube-downloader.section-videos
   (:require [clojure.java.shell :refer [sh]]
-            [clojure.java.io :refer [input-stream]]
-            [clojure.java.io :as io])
-  (:import (java.io ByteArrayOutputStream)))
+            [clojure.java.io :refer [input-stream]]))
 
 (defn section-exec
   [input {:keys [start end]}]
@@ -10,31 +8,6 @@
       :in input
       :out-enc :bytes))
 
-; only for testing, can delete
-(defn write-file [input]
-  (with-open [w (java.io.BufferedOutputStream. (java.io.FileOutputStream. "C:\\Users\\James\\Downloads\\test-section.mp4"))]
-    (.write w input)))
-
-(comment
-  (write-file
-    (:out
-      (section-exec (input-stream "C:\\Users\\James\\Downloads\\test.mp4") {:start 2 :end 4}))))
-
-; associates sh result with each section map
-; TODO put extension on :name ?  otherwise it's file won't have like .mp4
 (defn section-video
   [input sections]
   (map #(assoc % :result (section-exec input %)) sections))
-
-; only for testing, can delete
-(defn file->bytes [file]
-  (with-open [xin (io/input-stream file)
-              xout (ByteArrayOutputStream.)]
-    (io/copy xin xout)
-    (.toByteArray xout)))
-
-(comment
-  (section-video
-    (file->bytes "C:\\Users\\James\\Downloads\\test.mp4")
-    [{:name "name 1" :start 1 :end 2}
-     {:name "name 2" :start 3 :end 4}]))
